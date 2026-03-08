e9b1f85e6e9e31f9e720a80990f0691f1bda854c
Mottuqe Brid <mottuqebrid@gmail.com>
2026-03-08 16:26:05 +0600

    Refactor Logo component and update global styles for improved theming

--- src/_components/Logo/Logo.tsx
+++ src/_components/Logo/Logo.tsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Logo() {
+export default function Logo({
+  href,
+  classes,
+  size,
+  imgClass,
+}: {
+  href: string;
+  classes?: string;
+  size?: string;
+  imgClass?: string;
+}) {
+  // dark:invert
   return (
-    <div>
+    <Link href={href || "/"} className={classes}>
       <Image
-        className="dark:invert"
-        src="/logo.svg"
+        className={` ${size === "large" ? "w-40" : "w-24"} ${imgClass || ""}`}
+        src="/logo_t.png"
         alt="Next.js logo"
         width={100}
         height={20}
         priority
       />
-    </div>
+    </Link>
   );
 }
